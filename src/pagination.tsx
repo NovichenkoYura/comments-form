d5d0510ba791ae89cf665c09a9f32398bd1be28f
NovichenkoYura <novichenko.y@gmail.com>
2022-09-29 17:33:42 +0300

    pag

--- src/pagination.tsx
+++ src/pagination.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useAppSelector } from './app/hooks';
+import { number } from 'yup/lib/locale';
+import { useAppSelector, useAppDispatch } from './app/hooks';
+import { setCurrentPage } from './store/commentSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesLeft, faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 
 export interface paginationProps {
   currentpage?: number;
@@ -18,16 +22,31 @@ export const Pagination: React.FC<paginationProps> = () => {
     pageNumbers.push(i);
   }
 
+  const dispatch = useAppDispatch();
+  const paginate = (number: number) => dispatch(setCurrentPage(number));
+  const paginateDecriment = (currentPage: number) => dispatch(setCurrentPage(currentPage - 1));
+  const paginateIncriment = (currentPage: number) => dispatch(setCurrentPage(currentPage + 1));
+
   return (
     <div>
       <ul className="pagination">
+        <li className="page__item">
+          <a href="!#" className="page__link" onClick={() => paginateDecriment(currentPage)}>
+            <FontAwesomeIcon icon={faAnglesLeft} />
+          </a>
+        </li>
         {pageNumbers.map((number) => (
           <li className="page__item" key={number}>
-            <a href="!#" className="page__link">
+            <a href="!#" className="page__link" onClick={() => paginate(number)}>
               {number}
             </a>
           </li>
         ))}
+        <li className="page__item">
+          <a href="!#" className="page__link" onClick={() => paginateIncriment(currentPage)}>
+            <FontAwesomeIcon icon={faAnglesRight} />
+          </a>
+        </li>
       </ul>
     </div>
   );
